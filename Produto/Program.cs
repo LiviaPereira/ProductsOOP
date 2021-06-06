00dbb8be3d2951eecba069f41d4a66c3e7a1e4b5
Livia <liviapereira1997@hotmail.com>
2021-06-06 14:41:10 -0300

    Add project files.

---
+++ Produto/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+
+namespace Produto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre com os dados do produto: ");
+
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser adicionados no estoque: ");
+            int qnt = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qnt);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser removidos no estoque: ");
+            qnt = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qnt);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+
+
+
+
+        }
+    }
+}
